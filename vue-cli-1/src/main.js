34e60e84f4411c8e83646bd2122441b596d2138d
Lukman Nakib <lukman.nakib@gmail.com>
2019-09-29 17:04:22 +0600

    cli

--- vue-cli-1/src/main.js
+++ vue-cli-1/src/main.js
@@ -3,6 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 
+window.$ = window.jQuery = require('jquery')
+
+import 'bootstrap' 
+import 'bootstrap/dist/css/bootstrap.min.css'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
